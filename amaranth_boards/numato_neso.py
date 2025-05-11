4ebb4ab747d6ab48e24d745af8c974ebe827a5c7
Bastian Löher <b.loeher@gsi.de>
2025-05-12 00:02:43 +0200

    neso: fix pin assignments / naming.

--- amaranth_boards/numato_neso.py
+++ amaranth_boards/numato_neso.py
@@ -28,9 +28,16 @@ Example Usage:
     platform.build(Top(), do_program=True)
 
 Supported programmer:
-    tentative: openFPGAloader with arty_a7_100t or nexys_a7_100 board
-    requires custom cable (similar to 'numato'?):
+	openFPGALoader
+    requires cable 'numato-neso' or board 'neso_a7':
       FTDI_SER(0x2a19, 0x1005, FTDI_INTF_[A|B], 0x08, 0x4b, 0x00, 0x00)
+    board jumpers must be set for JTAG programming: 2-4, 3-5, 8-10, 9-11
+
+    programming flash (via spiOverJtag_xc7a100tcsg324.bit.gz):
+      openFPGALoader -b neso_a7 top.bit -f
+
+    programming sram:
+      openFPGALoader -b neso_a7 top.bit
 
 Voltage settings:
     set_property CFGBVS VCCO [current_design]
@@ -49,7 +56,7 @@ class NumatoNesoPlatform(XilinxPlatform):
     resources   = [
         # On-board oscillator
         Resource("clk100", 0, Pins("F4", dir="i"),
-                 Clock(100e6), Attrs(IOSTANDARD="LVCMOS33")),
+        	Clock(100e6), Attrs(IOSTANDARD="LVCMOS33")),
        
         # DDR3 RAM
         Resource("ddr3", 0,
@@ -85,19 +92,21 @@ class NumatoNesoPlatform(XilinxPlatform):
     connectors = [
         Connector("p", 4, # IOSTANDARD="LVCMOS33"
             """
-            A14 A13 D13 D12  A11 B11 F14 F13  B14 B13 A16 A15   A9 A10 B12 C12
-             A8  B8 C10 C11   B9  C9  B6  B7   C5  C6  A5  A6   C7  D8  D7  E7
-             D4  D5  D3  E3   A3  A4  B2  B3   C1  C2  A1  B1   G1  H1  E1  F1
-             D2  E2  K1  K2   J2  J3  B4  C4   E5  E6  G2  H2   F3  F5  G3  G4
-             H5  H6  H4  J4   F6  G6
+              -   -   -   -  A14 A13 D13 D12  A11 B11 F14 F13  B14 B13 A16 A15
+             A9 A10 B12 C12   A8  B8 C10 C11   B9  C9  B6  B7   C5  C6  A5  A6
+             C7  D8  D7  E7   D4  D5   -   -    -   -  D3  E3    -   -  A3  A4
+              -   -   -   -    -   -  B2  B3   C1  C2  A1  B1   G1  H1  E1  F1
+              -   -  D2  E2   K1  K2  J2  J3   B4  C4  E5  E6   G2  H2  F3  F5
+             G3  G4  H5  H6   H4  J4  F6  G6    -   -   -   -    -   -   -   -
             """),
         Connector("p", 5, # IOSTANDARD="LVCMOS33"
             """
-            B16 B17 D14 C14  C16 C17 H14 G14   T8  J5 E15 E16  E17 D17 F15 F16
-            J14 H15 H17 G17  H16 G16 K13 J13  L15 L16 L18 M18  R12 R13 K15 J15
-            M16 M17 R18 T18  P15 R15 N15 N16  N14 P14 P17 R17  N17 P18 U16 V17
-            U17 U18 U14 V14  V15 V16 T14 T15  R16 T16  T9 T10  T13 U13 T11 U11
-            R10 R11 V10 V11  U12 V12 
+              -   -   -   -   -   -  B16 B17    -   - D14 C14    -   - C16 C17
+            H14 G14  T8  J5  E15 E16 E17 D17  F15 F16 J14 H15  H17 G17 H16 G16
+            K13 J13 L15 L16  L18 M18 R12 R13  K15 J15 M16 M17    -   -   -   -
+              -   -   -   -  R18 T18 P15 R15  N15 N16 N14 P14  P17 R17 N17 P18
+            U16 V17 U17 U18  U14 V14 V15 V16  T14 T15 R16 T16   T9 T10 T13 U13
+            T11 U11 R10 R11  V10 V11 U12 V12    -   -   -   -    -   -   -   -
             """),
         # FTDI Chip
         Connector("ftdi", 0, { # IOSTANDARD="LVCMOS33"
@@ -105,11 +114,12 @@ class NumatoNesoPlatform(XilinxPlatform):
             "d1": "B18",
             "d2": "D18",
             "d3": "E18",
-            "d4": "F18",
+            "d4": "F18", # manual mentions F1 for rev 1
             "d5": "G18",
             "d6": "J17",
             "d7": "J18",
-            "oe_n": "C15",
+            "oe_n": "C15",    # only rev 2
+            "clkout": "D15",  # only rev 2
             "txe_n": "K16",
             "rxf_n": "G13",
             "wr_n": "M13",
